7e0876deb40c28292456aebe248c16bbb82c4836
Sudo-Si <dalgleishmaswabi@gmail.com>
2022-11-21 18:45:46 +1300

    first heroku commit

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-    baseURL : "https://jab.herokuapp.com/api/"
-})
+    baseURL : "https://just-anotherblog.herokuapp.com/api/"
+});
